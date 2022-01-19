61381631e7c9e3ea640790994028bac394558905
mtk246 <minthukyaw454@gmail.com>
2022-01-19 22:02:27 +0630

    bug fix for https content error

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\URL;
 
+if (App::environment('production')) {
+    URL::forceScheme('https');
+}
 /*
 |--------------------------------------------------------------------------
 | Web Routes
