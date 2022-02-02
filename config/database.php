859a8560a5933dd367912c07cee6830a630df3b0
mtk246 <minthukyaw454@gmail.com>
2022-02-02 22:56:46 +0630

    Group By query fix in LineEntryController

--- config/database.php
+++ config/database.php
@@ -73,7 +73,7 @@ return [
             'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8',
             'prefix' => '',
-            'prefix_indexes' => true, 'strict' => false,
+            'prefix_indexes' => true,
             'schema' => 'public',
             'sslmode' => 'prefer',
         ],
