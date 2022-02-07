2026c74de662c4823f5393c28ca8bb57d8223621
mtk246 <minthukyaw454@gmail.com>
2022-02-07 21:40:24 +0630

    Live Dash Percent Chart Complete and edit date in LiveDashController query

--- app/Charts/LiveDashPercentChart.php
+++ app/Charts/LiveDashPercentChart.php
@@ -6,6 +6,7 @@ use ArielMejiaDev\LarapexCharts\LarapexChart;
 use App\Models\Line;
 use App\Models\Time;
 use Illuminate\Support\Facades\DB;
+use App\Models\LineAssign;
 
 class LiveDashPercentChart
 {
@@ -18,13 +19,17 @@ class LiveDashPercentChart
 
     public function build(): \ArielMejiaDev\LarapexCharts\HorizontalBar
     {
+        $date_string = date("d.m.Y");
+        $line_assign = LineAssign::select('main_target')->orderBy('l_id', 'asc')->where('assign_date', $date_string)->pluck('main_target')->toArray();
+        $line = Line::select('l_name')->where('a_status', 1)->orderBy('l_pos', 'asc')->pluck('l_name')->toArray();
+        $time = Time::select(DB::raw('line_id,sum(div_actual_target) as total_actual_target'))->groupBy('line_id')->orderBy('line_id', 'asc')->pluck('total_actual_target')->toArray();
         return $this->percent_chart->horizontalBarChart()
-            ->setTitle('Los Angeles vs Miami.')
-            ->setSubtitle('Wins during season 2021.')
+            ->setTitle('Target and Actual Target')
+            // ->setSubtitle('Wins during season 2021.')
             ->setColors(['#FFC107', '#D32F2F'])
-            ->addData('Actual Target', [6, 9, 3, 4, 10, 8])
-            ->addData('Target', [Time::select('div_target')->orderBy('line_id', 'asc')->pluck('div_target')->toArray()])
-            ->setXAxis([Line::select('l_name')->where('a_status', 1)->orderBy('l_pos', 'asc')->pluck('l_name')->toArray()]);
+            ->addData('Actual Target', $time)
+            ->addData('Target', $line_assign)
+            ->setXAxis($line);
         // ->groupBy('line_id')->orderBy('line_id', 'asc')
     }
 }
--- app/Http/Controllers/LiveDashController.php
+++ app/Http/Controllers/LiveDashController.php
@@ -22,30 +22,39 @@ class LiveDashController extends Controller
     }
     public function index(LiveDashPercentChart $percent_chart)
     {
+        $date_string = date("d.m.Y");
         $u_id = Auth::user()->id;
-        $responseBody = DB::select('SELECT "line".l_id,"line".l_name,"line_assign".assign_id,"line_assign".main_target,"line_assign".s_time,"line_assign".e_time,"line_assign".lunch_s_time,"line_assign".lunch_e_time,"line_assign".assign_date,"time".time_id,"time".time_name,"time".status,"time".div_target,"time".actual_target_entry,"time".div_actual_target,"time".div_actual_percent
-        FROM line
-        JOIN line_assign ON "line_assign".l_id = "line".l_id
-        JOIN time ON "time".line_id = "line_assign".l_id
-               ORDER BY "time".time_id ASC');
-        $p_detail = ProductDetail::select(
-            'p_detail_id',
-            'assign_id',
-            'l_id',
-            'p_cat_id',
-            'p_name',
-            'quantity'
-        )->orderBy('p_detail_id', 'asc')->get();
-        $line = Line::where('a_status', 1)->orderBy('l_pos', 'asc')->get();
-        $time = Time::select('time_name')->groupBy('time_name')->orderBy('time_name', 'asc')->get();
-        $time_2 = Time::select('time_id', 'time_name', 'line_id', 'assign_id', 'status', 'div_target', 'div_actual_target', 'div_actual_percent', 'actual_target_entry')->orderBy('time_id', 'asc')->get();
+
+        $responseBody = DB::select('SELECT "line".l_id,"line".l_name,"line_assign".assign_id,"line_assign".main_target,
+        "line_assign".s_time,"line_assign".e_time,"line_assign".lunch_s_time,"line_assign".lunch_e_time,
+        "line_assign".assign_date,"time".time_id,"time".time_name,"time".status,"time".div_target,
+        "time".actual_target_entry,"time".div_actual_target,"time".div_actual_percent
+                FROM line
+                JOIN line_assign ON "line_assign".l_id = "line".l_id AND "line_assign".assign_date=\'' . $date_string . '\'
+                JOIN time ON "time".line_id = "line_assign".l_id ORDER BY "time".time_id ASC');
+
+        $p_detail = DB::select('SELECT "p_detail".p_detail_id,"p_detail".assign_id,"p_detail".l_id,"p_detail".p_cat_id,"p_detail".p_name,"p_detail".quantity FROM p_detail,line_assign WHERE "p_detail".assign_id="line_assign".assign_id AND "line_assign".assign_date=\'' . $date_string . '\' ORDER BY "p_detail".p_detail_id ASC');
+        // ProductDetail::select(DB::raw(
+        //     '"p_detail".p_detail_id,"p_detail".assign_id,"p_detail".l_id,"p_detail".p_cat_id,"p_detail".p_name,"p_detail".quantity'
+        // ))->orderBy('p_detail_id', 'asc')->get();
+
+        $line = DB::select('SELECT "line".l_id,"line".l_name FROM line,line_assign WHERE "line".a_status=1 AND "line".is_delete=0 AND "line".l_id="line_assign".l_id AND "line_assign".assign_date=\'' . $date_string . '\' ORDER BY "line".l_pos ASC');
+        // Line::table()->where('a_status', 1)->orderBy('l_pos', 'asc')->get();
+
+        $time = DB::select('SELECT "time".time_name FROM time,line_assign WHERE "time".assign_id="line_assign".assign_id AND "line_assign".assign_date=\'' . $date_string . '\' ORDER BY "time".time_id ASC');
+        // Time::select('time_name')->groupBy('time_name')->orderBy('time_name', 'asc')->get();
+
+        $time_2 = DB::select('SELECT "time".time_id,"time".time_name,"time".line_id,"time".assign_id,"time".status,"time".div_target,"time".div_actual_target,"time".div_actual_percent,"time".actual_target_entry FROM time,line_assign WHERE "time".assign_id="line_assign".assign_id AND "line_assign".assign_date=\'' . $date_string . '\' ORDER BY "time".time_id ASC');
+        //  Time::select('time_id', 'time_name', 'line_id', 'assign_id', 'status', 'div_target', 'div_actual_target', 'div_actual_percent', 'actual_target_entry')->orderBy('time_id', 'asc')->get();
+
         $getLine = DB::select('SELECT "line".l_id,"line".l_name,"line_assign".assign_id,"line_assign".main_target,"line_assign".s_time,"line_assign".e_time,"line_assign".lunch_s_time,"line_assign".lunch_e_time,"line_assign".assign_date,"users".id,"users".name
         FROM line
         JOIN line_assign ON "line_assign".l_id = "line".l_id
         JOIN users ON "users".id= "line_assign".user_id
-        WHERE "line".a_status=1 ORDER BY "line".l_pos ASC');
+        WHERE "line".a_status=1 AND "line_assign".assign_date=\'' . $date_string . '\' ORDER BY "line".l_pos ASC');
+
         $top_line = DB::select('SELECT "time".line_id,"line".l_name,SUM("time".div_actual_target) AS total_actual
-        FROM time,line WHERE "time".line_id="line".l_id AND div_actual_percent IS NOT NULL GROUP BY
+        FROM time,line,line_assign WHERE "line_assign".assign_date=\'' . $date_string . '\' AND "time".line_id="line".l_id AND "time".line_id="line_assign".l_id AND div_actual_percent IS NOT NULL GROUP BY
         "time".line_id,"line".l_name ORDER BY SUM("time".div_actual_target) DESC LIMIT 3');
 
         DB::disconnect('musung');
