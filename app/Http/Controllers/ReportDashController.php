81082ed7d9bd1412dd260bcb38901db49d26fad8
mtk246 <minthukyaw454@gmail.com>
2022-02-16 16:06:16 +0630

    Login Fix v.2

--- app/Http/Controllers/ReportDashController.php
+++ app/Http/Controllers/ReportDashController.php
@@ -7,6 +7,7 @@ use App\Charts\ReportDashCategoryChart;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class ReportDashController extends Controller
 {
@@ -21,6 +22,20 @@ class ReportDashController extends Controller
     }
     public function index(ReportDashAreaChart $chart, ReportDashCategoryChart $category_chart)
     {
-        return view('report_management.report', ['chart' => $chart->build(), 'category_chart' => $category_chart->build()]);
+        $category = DB::select('SELECT p_cat_id,SUM(cat_actual_target) AS t_cat_actual,p_name FROM p_detail
+        WHERE DATE(created_at) >= DATE(NOW()) - INTERVAL \'30\' DAY
+		GROUP BY p_cat_id,p_name');
+
+        $target = DB::select('SELECT "line_assign".assign_date,SUM("line_assign".main_target) AS t_main_target FROM line_assign
+        WHERE DATE("line_assign".created_at) >= DATE(NOW()) - INTERVAL \'30\' DAY
+        GROUP BY "line_assign".assign_date ORDER BY "line_assign".assign_date ASC');
+
+        $time = DB::select('SELECT SUM("time".div_actual_target) AS t_actual_target FROM time
+        WHERE DATE("time".created_at) >= DATE(NOW()) - INTERVAL \'30\' DAY
+        GROUP BY "time".assign_date ORDER BY "time".assign_date ASC');
+
+        DB::disconnect('musung');
+
+        return view('report_management.report', ['chart' => $chart->build(), 'category_chart' => $category_chart->build()], compact('category', 'target', 'time'));
     }
 }
--- resources/views/report_management/report.blade.php
+++ resources/views/report_management/report.blade.php
@@ -5,17 +5,149 @@
 @section('content_2')
 
 <div class="row container-fluid">
-    <div class="col-12 col-md-6 my-4 bg-white rounded shadow">
-        {!! $chart->container() !!}
+    <div class="col-12 col-md-6 my-4 bg-white rounded shadow" id="production_chart">
+        {{-- {!! $chart->container() !!} --}}
     </div>
-    <div class="col-12 col-md-6 my-4">
-        {!! $category_chart->container() !!}
+    <div class="col-12 col-md-6 my-4" id="product_chart">
+        {{-- {!! $category_chart->container() !!} --}}
     </div>
 </div>
-<script src="{{ $chart->cdn() }}"></script>
+{{-- <script src="{{ $chart->cdn() }}"></script>
 {{ $chart->script() }}
 <script src="{{ $category_chart->cdn() }}"></script>
-{{ $category_chart->script() }}
+{{ $category_chart->script() }} --}}
+
+<script>
+    var options = {
+    series: [{
+    name: 'Target',
+    data: [
+    @foreach($target as $t)
+    @php
+    $t_main_target = $t->t_main_target;
+    echo $t_main_target . ',';
+    @endphp
+    @endforeach]
+    }, {
+    name: 'Actual Target',
+    data: [@foreach($time as $t)
+@php
+$t_div_actual_target = $t->t_actual_target;
+echo $t_div_actual_target . ',';
+@endphp
+@endforeach]
+    }],
+    title: {
+    text: 'Production Report of last 30 days',
+    align: 'left',
+    margin: 10,
+    offsetX: 0,
+    offsetY: 0,
+    floating: false,
+    style: {
+      fontSize:  '14px',
+      fontWeight:  'bold',
+      fontFamily:  'Arial',
+      color:  '#263238'
+    },
+},
+    chart: {
+    height: 350,
+    type: 'area',toolbar: {
+    show: true
+},
+    },
+    dataLabels: {
+    enabled: true,
+    },
+    stroke: {
+    curve: 'smooth'
+    },
+    xaxis: {
+    categories: [
+    @foreach($target as $t_2)
+    @php
+    $assign_date = $t_2->assign_date;
+    $month = date('m', strtotime($assign_date)); // Create date object to store the DateTime format
+    $day = date('d', strtotime($assign_date));
+    $dateObj = DateTime::createFromFormat('!m', $month);
+
+    // Store the month name to variable
+    $monthName = $dateObj->format('F');
+    $full_format = $day . ' ' . $monthName;
+
+    echo '"' . $full_format . '"' . ',';;
+    @endphp
+    @endforeach]
+    },
+    tooltip: {
+    x: {
+
+    },
+    },
+    };
+
+    var chart = new ApexCharts(document.querySelector("#production_chart"), options);
+    chart.render();
+</script>
+
+<script>
+    var options = {
+  series: [@foreach($category as $c)
+@php
+$cat_actual = $c->t_cat_actual;
+if($cat_actual == ''){
+$cat_actual = 0;
+}
+if($cat_actual!=''){
+$cat_actual = $cat_actual;
+}
+echo $cat_actual . ',';
+@endphp
+@endforeach],
+  chart: {
+      width:'100%',
+      height:'80%',
+      type: 'pie',toolbar: {
+    show: true
+},
+},title: {
+    text: 'Production Report of Categories for 30 days',
+    align: 'left',
+    margin: 10,
+    offsetX: 0,
+    offsetY: 0,
+    floating: false,
+    style: {
+      fontSize:  '14px',
+      fontWeight:  'bold',
+      fontFamily:  'Arial',
+      color:  '#263238'
+    },
+},
+labels: [
+@foreach($category as $c)
+@php
+$p_name = $c->p_name;
+echo '"' . $p_name . '"' . ',';
+@endphp
+@endforeach],
+responsive: [{
+  breakpoint: 480,
+  options: {
+    chart: {
+    //   width: 200
+    },
+    legend: {
+      position: 'bottom'
+    }
+  }
+}]
+};
+
+var chart = new ApexCharts(document.querySelector("#product_chart"), options);
+chart.render();
+</script>
 @endsection
 
 @endsection
