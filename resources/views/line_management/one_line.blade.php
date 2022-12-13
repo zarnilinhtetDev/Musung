2e0eed268599181d2daa3417262404f9bbd552d7
kaungkaung4T <mrkaungminnkhant@gmail.com>
2022-12-13 11:00:01 +0630

    update

--- resources/views/line_management/one_line.blade.php
+++ resources/views/line_management/one_line.blade.php
@@ -8,7 +8,54 @@
 <link href="{{ URL::asset('css/styles.css') }}" rel="stylesheet"> --}}
 
 
+
 <div class="container-fluid">
+
+
+
+
+
+
+
+    @php $date_string = date("d.m.Y");
+    $date_string_for_export_pdf = date("Y_m_d", strtotime($date_string)); @endphp
+    <div class="row container-fluid pt-4">
+        <div class="col-12 col-md-6 m-auto">
+            <ul class="horizontal-slide" style="" id="tabs">
+                <li class="span2 bg-transparent">
+                    <h2 class="m-0 fw-bold">Date - {{ $date_string }}</h2>
+                </li>
+            </ul>
+        </div>
+        <div class="col-12 col-md-6 my-auto text-center text-md-start p-0">
+            <div>
+                <h2 class="m-0 fw-bold" id="digital-clock-2"></h2>
+            </div>
+            <script>
+                /// Live Clock in line_entry.blade
+            function showTime() {
+                var date = new Date().toLocaleTimeString(
+                    "en-US",
+                    Intl.DateTimeFormat().resolvedOptions().timeZone
+                );
+                document.getElementById("digital-clock-2").innerHTML = date;
+            }
+            setInterval(showTime, 1000);
+            /// Live Clock in line_entry.blade End
+            </script>
+        </div>
+    </div>
+
+
+
+
+
+
+
+
+
+
+
 
     <div id="history_div">
         <div class="row container-fluid p-0 my-3 mx-auto">
